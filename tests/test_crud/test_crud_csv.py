6898f027507823023bc23f768bf854135165f7b3
Lilstella <luisstella.m@gmail.com>
2025-07-31 21:42:59 -0300

    Add test for crud of txt and csv files

--- tests/test_crud/test_crud_csv.py
+++ tests/test_crud/test_crud_csv.py
@@ -4,38 +4,38 @@ from app.main import app
 
 client = TestClient(app)
 
-def test_crud_markdown():
-    filename = "testfile_markdown_crud"
-    content = {"text": "Hi **world**", "filetype": "markdown"}
+def test_crud_csv():
+    filename = "testfile_csv_crud"
+    content = {"text": "name,age\nmaria,87", "filetype": "csv"}
 
     # Create file
     response = client.post(f"/file/create/{filename}", json=content)
     assert response.status_code == 201
     assert "created" in response.json()["message"]
-    assert os.path.exists(f"storage/markdown/{filename}.md")
+    assert os.path.exists(f"storage/csv/{filename}.csv")
 
     # Read file
-    response = client.request(method="GET", url=f"/file/{filename}", params={"file_type": "markdown"})
+    response = client.request(method="GET", url=f"/file/{filename}", params={"file_type": "csv"})
     assert response.status_code == 200
     assert response.json()["content"] == content["text"]
     assert response.json()["filename"] == filename
-    assert os.path.exists(f"storage/markdown/{filename}.md")
+    assert os.path.exists(f"storage/csv/{filename}.csv")
 
     # Actualize file
-    new_content = {"text": "Hola **mundo** actualizado", "filetype": "markdown"}
+    new_content = {"text": "name,age\nmaria,87\nclara,43", "filetype": "csv"}
     response = client.put(f"/file/edit/{filename}", json=new_content)
     assert response.status_code == 200
     assert "updated" in response.json()["message"]
-    assert os.path.exists(f"storage/markdown/{filename}.md")
+    assert os.path.exists(f"storage/csv/{filename}.csv")
     
-    response = client.request(method="GET", url=f"/file/{filename}", params={"file_type": "markdown"})
+    response = client.request(method="GET", url=f"/file/{filename}", params={"file_type": "csv"})
     assert response.status_code == 200
     assert response.json()["content"] == new_content["text"]
     assert response.json()["filename"] == filename
 
     # Erase file
-    response = client.request(method="DELETE", url=f"/file/delete/{filename}", params={"file_type": "markdown"})
+    response = client.request(method="DELETE", url=f"/file/delete/{filename}", params={"file_type": "csv"})
     print(response.json())
     assert response.status_code == 200
     assert "deleted" in response.json()["message"]
-    assert not os.path.exists(f"storage/markdown/{filename}.md")
+    assert not os.path.exists(f"storage/csv/{filename}.csv")
--- tests/test_crud/test_crud_txt.py
+++ tests/test_crud/test_crud_txt.py
@@ -4,38 +4,38 @@ from app.main import app
 
 client = TestClient(app)
 
-def test_crud_markdown():
-    filename = "testfile_markdown_crud"
-    content = {"text": "Hi **world**", "filetype": "markdown"}
+def test_crud_txt():
+    filename = "testfile_txt_crud"
+    content = {"text": "Hi world", "filetype": "txt"}
 
     # Create file
     response = client.post(f"/file/create/{filename}", json=content)
     assert response.status_code == 201
     assert "created" in response.json()["message"]
-    assert os.path.exists(f"storage/markdown/{filename}.md")
+    assert os.path.exists(f"storage/txt/{filename}.txt")
 
     # Read file
-    response = client.request(method="GET", url=f"/file/{filename}", params={"file_type": "markdown"})
+    response = client.request(method="GET", url=f"/file/{filename}", params={"file_type": "txt"})
     assert response.status_code == 200
     assert response.json()["content"] == content["text"]
     assert response.json()["filename"] == filename
-    assert os.path.exists(f"storage/markdown/{filename}.md")
+    assert os.path.exists(f"storage/txt/{filename}.txt")
 
     # Actualize file
-    new_content = {"text": "Hola **mundo** actualizado", "filetype": "markdown"}
+    new_content = {"text": "Hola mundo actualizado", "filetype": "txt"}
     response = client.put(f"/file/edit/{filename}", json=new_content)
     assert response.status_code == 200
     assert "updated" in response.json()["message"]
-    assert os.path.exists(f"storage/markdown/{filename}.md")
+    assert os.path.exists(f"storage/txt/{filename}.txt")
     
-    response = client.request(method="GET", url=f"/file/{filename}", params={"file_type": "markdown"})
+    response = client.request(method="GET", url=f"/file/{filename}", params={"file_type": "txt"})
     assert response.status_code == 200
     assert response.json()["content"] == new_content["text"]
     assert response.json()["filename"] == filename
 
     # Erase file
-    response = client.request(method="DELETE", url=f"/file/delete/{filename}", params={"file_type": "markdown"})
+    response = client.request(method="DELETE", url=f"/file/delete/{filename}", params={"file_type": "txt"})
     print(response.json())
     assert response.status_code == 200
     assert "deleted" in response.json()["message"]
-    assert not os.path.exists(f"storage/markdown/{filename}.md")
+    assert not os.path.exists(f"storage/txt/{filename}.txt")
