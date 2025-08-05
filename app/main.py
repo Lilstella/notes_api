0b8537e28c0111b3d4dfeeb413fcc53f4747bb5e
Lilstella <luisstella.m@gmail.com>
2025-08-05 19:43:02 -0300

    Rename crud to file for clarity

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
-from app.routes import crud, version, render, images, importer
+from app.routes import file, version, render, images, importer
 
 app = FastAPI(
     title="Markdown Editor API", description="API for manage MArkdowns", version="1.0.0"
 )
 
 # Routers
-app.include_router(crud.router, prefix="/file", tags=["Crud"])
+app.include_router(file.router, prefix="/file", tags=["File"])
 app.include_router(version.router, prefix="/file", tags=["Version"])
 app.include_router(render.router, prefix="/render", tags=["Render"])
 app.include_router(importer.router, prefix="/import", tags=["Import"])
