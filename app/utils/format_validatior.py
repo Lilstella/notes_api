d7223563e11e0efed6772dc65aa9264031cf27c0
Lilstella <luisstella.m@gmail.com>
2025-08-08 13:47:55 -0300

    Rename the functions of format

--- app/utils/format_validatior.py
+++ app/utils/format_validatior.py
@@ -3,29 +3,29 @@ import re
 def correct_format(content: str, file_type: str) -> bool:
     match file_type:
         case "markdown":
-            return is_markdown(content)
+            return format_markdown(content)
         case "csv":
-            return is_csv(content)
+            return format_csv(content)
         case "tsv":
-            return is_tsv(content)
+            return format_tsv(content)
         case "txt":
-            return is_txt(content)
+            return format_txt(content)
         case "html":
-            return is_html(content)
+            return format_html(content)
         case _:
             raise ValueError
         
-def is_markdown(content: str) -> bool:
+def format_markdown(content: str) -> bool:
     pass
 
-def is_csv(content: str) -> bool:
+def format_csv(content: str) -> bool:
     pass
 
-def is_tsv(content: str) -> bool:
+def format_tsv(content: str) -> bool:
     pass
 
-def is_txt(content: str) -> bool:
+def format_txt(content: str) -> bool:
     pass
 
-def is_html(content: str) -> bool:
+def format_html(content: str) -> bool:
     pass 
